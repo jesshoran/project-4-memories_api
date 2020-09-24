38dd77a500e3761fd77e9d42a04e29f5f7585664
lakergirl702 <jesshoran87@gmail.com>
2020-09-24 11:05:18 -0700

    deployment

---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Event.create([
+    {
+        "name": "Coachella 2015",
+        "date": "4/12/15 - 4/14/15",
+        "memory": "This was my first time going with Greg, Jose and co! What a great time we had! Highlights were Empire of the Sun and Gorgon City!"
+    },
+    {
+        "name": "Lakers / Celtics in Boston",
+        "date": "3/18/2008",
+        "memory": "I was in college and my mom got me this amazing gift for my birthday! I went with Bryant and LAKERS WON! Best day ever!"
+    },
+    {
+        "name": "Cruise through the Panama Canal",
+        "date": "January 2020",
+        "memory": "I can't believe I was able to do this right before COVID began! Jordan and I joined my parents on this amazing cruise throught the Panama Canal. We met Natalie and Luke...lifelong friends!"
+    },
+    {
+        "name": "My 30th Birthday in Hawaii",
+        "date": "December 2007",
+        "memory": "This was maybe my most amazing birthday! Greg, Rob, Dan, Jason, Missy and Eva all came out to celebrate! Barry took us all to Wolfgang's (my fave spot in Hawaii) and it was an amazing time."
+    },
+    {
+        "name": "Julian's Wedding",
+        "date": "November 2, 2019",
+        "memory": "I was a groomsman for the first time ever! It was great to stand by my brother's side on his special day. I gave a killer speech too!"
+    }
+])
